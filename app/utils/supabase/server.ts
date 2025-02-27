992ead7c4b7689f8cceee5e7848b8ce68d7fe4f9
Anderson Costa <costaandersom23@gmail.com>
2025-02-27 17:47:45 -0300

    fix coretion error

--- app/utils/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-
-export const createClient = (cookieStore: unknown) => {
-  return createServerComponentClient({ cookies });
-};

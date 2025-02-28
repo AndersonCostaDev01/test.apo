a9e3d6645717e8a7f5a9f2d5627cab6ff15fc553
Anderson Costa <costaandersom23@gmail.com>
2025-02-28 10:51:45 -0300

    feat restuture project cleat all

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-import Link from "next/link";
-
 function Home() {
-  return (
-    <div className="w-screen h-screen bg-zinc-400">
-      <div className="p-20 w-full text-center">
-        <h1 className="font-black text-5xl">Teste de api</h1>
-      </div>
-      <div className="p-10 flex gap-12">
-        <Link
-          className="px-4 py-2 bg-zinc-700 rounded-2xl hover:bg-zinc-500 transition"
-          href="/register"
-        >
-          Registro
-        </Link>
-        <Link
-          className="px-4 py-2 bg-zinc-700 rounded-2xl hover:bg-zinc-500 transition"
-          href="/users"
-        >
-          Usuarios
-        </Link>
-      </div>
-    </div>
-  );
+  return <div>test</div>;
 }
 
 export default Home;
